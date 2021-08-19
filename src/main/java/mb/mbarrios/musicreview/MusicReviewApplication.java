1283eb68024b403c473a75a3de36929e3245d854
Miguel Barrios <miguelbh440@gmail.com>
2021-08-19 12:24:18 -0500

    first commit

---
+++ src/main/java/mb/mbarrios/musicreview/MusicReviewApplication.java
@@ -0,0 +1,13 @@
+package mb.mbarrios.musicreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mb/mbarrios/musicreview/MusicReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package mb.mbarrios.musicreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
